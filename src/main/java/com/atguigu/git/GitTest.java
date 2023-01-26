d29d4a9d6b7eb2000f8bf63ceb3bdc11f20d63fc
K_Asahi <1421064569@qq.com>
2023-01-26 14:48:56 +0800

    hot-fix commit2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git3");
         System.out.println("hello git4");
+        System.out.println("hot-fix test!");
     }
 }
