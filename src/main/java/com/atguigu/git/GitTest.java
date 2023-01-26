4aa50bd370b69733d1ecbe95cec903c4d4f7ec7d
K_Asahi <1421064569@qq.com>
2023-01-26 14:12:59 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author 天涯恒梦
+ * @create 2023-01-26 14:05
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
